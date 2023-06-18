45127da77c1d4ea742858396e289c3f54080ee8e
ibado <ignaciobado.unc@gmail.com>
2023-06-18 18:41:24 +0200

    Implement android client connection

--- client/android/app/src/main/java/com/example/my_ws_chat_client/MainActivity.kt
+++ client/android/app/src/main/java/com/example/my_ws_chat_client/MainActivity.kt
@@ -1,60 +1,103 @@
 package com.example.my_ws_chat_client
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.example.my_ws_chat_client.MessagesService.*
 import com.example.my_ws_chat_client.ui.theme.MywschatclientTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val messages = mutableStateListOf<Message>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val messagesService = MessagesService()
+
+        lifecycleScope.launchWhenCreated {
+            messagesService.use { service ->
+                service.initChat { message ->
+                    Log.i("DROID", "new data: $message")
+                    messages.add(message)
+                }
+            }
+        }
+
         setContent {
             MywschatclientTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize().padding(10.dp),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    ChatView(
-                        listOf(
-                            Message("Hello there!", MsgType.ME),
-                            Message("How're you?", MsgType.ME),
-                            Message("I'm doing well", MsgType.OTHER),
-                            Message("What about you?", MsgType.OTHER)
-                        )
-                    )
+                    ChatView(messages)
+                    MessageBar { lifecycleScope.launch { messagesService.sendMessage(it) } }
                 }
+            }
+        }
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    private fun MessageBar(sendMessage: (String) -> Unit) {
+        Row(verticalAlignment = Alignment.Bottom) {
+            var textValue by remember { mutableStateOf(TextFieldValue("")) }
+            TextField(value = textValue, onValueChange = {
+                textValue = it
+            })
+            Button(onClick = {
+                if (textValue.text.isNotBlank()) {
+                    sendMessage(textValue.text)
+                    textValue = TextFieldValue("")
+                }
+            }, modifier = Modifier.padding(5.dp)) {
+                Text(text = "Send")
             }
         }
     }
 }
 
-enum class MsgType {
-    ME, OTHER;
-}
-
-data class Message(val content: String, val type: MsgType)
-
 @Composable
-fun ChatView(messages: List<Message>, modifier: Modifier = Modifier) =
-    LazyColumn(modifier = modifier) {
+fun ChatView(messages: SnapshotStateList<Message>, modifier: Modifier = Modifier) =
+    LazyColumn(modifier = modifier, contentPadding = PaddingValues(bottom = 50.dp)) {
         items(messages.size) {
             val msg = messages[it]
             val ta = if (msg.type == MsgType.ME) TextAlign.End else TextAlign.Start
             Text(
                 msg.content,
                 textAlign = ta,
-                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
             )
         }
     }
